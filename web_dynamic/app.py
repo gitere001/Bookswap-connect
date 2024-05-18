6f40de2afbe4b0d9c2b81672a3f337770a0ec50c
James gitere <sasmuypwa2019.com>
2024-05-18 18:23:12 +0300

    updated flask to handle route signup

--- web_dynamic/app.py
+++ web_dynamic/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
@@ -9,12 +9,21 @@ def landing_page():
 
 @app.route('/login', strict_slashes=False)
 def login_page():
-    return ("comming soon..")
+    return (render_template('login.html'))
 
 
 @app.route('/signup', strict_slashes=False)
 def sign_up_page():
-    return ("coming soon..")
+    return (render_template('sign_up.html'))
+
+
+@app.route('/sign_up', strict_slashes=False, methods=['POST'])
+def signup():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    password = request.form['password']
+    confirm_password = request.form['confirm_password']
 
 
 if __name__ == "__main__":
