0197f2834aec486f09dc77218351e9013c31879e
James gitere <sasmuypwa2019.com>
2024-05-20 20:32:49 +0300

    updated user model

--- models/user.py
+++ models/user.py
@@ -1,18 +1,23 @@
-#!/usr/bin/python3
-"""this is the user model for the user of bookswap connect"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import models
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """User class"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False, unique=True)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
 
-    def __init__(self):
+    def __init__(self, email, password, first_name, last_name):
+        """Initialize the user with email, password, first name, and last
+        name."""
         super().__init__()
-
-
-user1 = User()
-print(user1)
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
